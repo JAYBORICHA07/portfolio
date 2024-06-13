966eb9b5e60f2e94df5fe061a194f01f203d6a06
JAYBORICHA <jayboricha707@gmail.com>
2024-06-14 03:23:25 +0530

    responsive done

--- src/components/playground/rifle-1.tsx
+++ src/components/playground/rifle-1.tsx
@@ -76,8 +76,7 @@ const Illustrations = () => {
     <div class="flex flex-col justify-center w-fit h-fit text-white p-4">
       <h1>Rifle animation</h1>
       <p>
-        Interactive animation made in rive.app using an
-        illustration made by me
+        Interactive animation made in rive.app using an illustration made by me
       </p>
       <p>
         click on the rifle or on the <span class="font-bold">S</span> and on the{" "}
